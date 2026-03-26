fe1c77b0775f946efaa85207648bd982a0965ed5
Loïs <lois.leblond2305@gmail.com>
2026-03-26 19:44:13 +0100

    fix hashage mdp et ajout formulaire connexion

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
+session_start();
 require_once 'config.php'; 
 
-$message_status = "";
+$message_status = $_SESSION['message_status'] ?? "";
+if (isset($_SESSION['message_status'])) {
+    unset($_SESSION['message_status']);
+}
 
 if (isset($_POST['register'])) {
     
-    $nom = $_POST['utilisateur'];
-    $mdp = $_POST['mdp'];
+    $nom = trim($_POST['utilisateur'] ?? '');
+    $mdp = $_POST['mdp'] ?? '';
 
-    $commande = "py script.py hash " . escapeshellarg($mdp);
-    $sortie = shell_exec($commande . ' 2>&1');
+    $pythonPath = "C:\\Users\\loisl\\AppData\\Local\\Programs\\Python\\Python38\\python.exe";
+
+    $commande = "$pythonPath script.py hash " . escapeshellarg($mdp) . " 2>&1";
+    $sortie = shell_exec($commande);
     
     $mdp_hache = trim($sortie ?? '');
 
@@ -20,7 +26,8 @@ if (isset($_POST['register'])) {
                 'nom' => $nom,
                 'mdp' => $mdp_hache
             ]);
-            $message_status = "$nom est bien enregistré";
+            header('Location: index.php');
+            exit;
         } catch (Exception $e) {
             $message_status = "Erreur BDD : " . $e->getMessage();
         }
@@ -48,6 +55,18 @@ if (isset($_POST['register'])) {
             <input type="password" name="mdp" required>
         <br>
         <button type="submit" name="register">S'inscrire</button>
+    </form>
+    </div>
+    <div class="formulaire">
+    <h1>Connexion</h1>
+    <form class="form" method="POST">
+        <label for="">Nom d'utilisateur</label>
+            <input type="text" name="utilisateur" required>
+        <br>
+        <label for="">Mot de passe</label>
+            <input type="password" name="mdp" required>
+        <br>
+        <button type="submit" name="login">Se connecter</button>
     </form>
     </div>
 </body>
--- script.py
+++ script.py
@@ -1,17 +1,30 @@
 import sys
-import bcrypt
-import uuid
-from cryptography.fernet import Fernet
+import hashlib
 
-if sys.argv[1] == "hash":
+try:
+    import bcrypt
+except ModuleNotFoundError:
+    bcrypt = None
+
+mode = sys.argv[1]
+
+if mode == 'hash':
     mdp = sys.argv[2].encode('utf-8')
-    hash_mdp = bcrypt.hashpw(mdp, bcrypt.gensalt())
-    print(hash_mdp.decode('utf-8'))
-
-elif sys.argv[1] == "check":
-    mdp_tape = sys.argv[2].encode('utf-8')
-    hash_bdd = sys.argv[3].encode('utf-8')
-    if bcrypt.checkpw(mdp_tape, hash_bdd):
-        print("Mot de passe correct")
+    if bcrypt:
+        hash_mdp = bcrypt.hashpw(mdp, bcrypt.gensalt()).decode('utf-8')
     else:
-        print("Mot de passe incorrect")
+        hash_mdp = hashlib.sha256(mdp).hexdigest()
+    print(hash_mdp)
+
+elif mode == 'check':
+    mdp_tape = sys.argv[2].encode('utf-8')
+    hash_bdd = sys.argv[3]
+    if bcrypt and hash_bdd.startswith('$2'):
+        ok = bcrypt.checkpw(mdp_tape, hash_bdd.encode('utf-8'))
+    else:
+        ok = hashlib.sha256(mdp_tape).hexdigest() == hash_bdd
+    print('Mot de passe correct' if ok else 'Mot de passe incorrect')
+
+else:
+    print('Erreur: mode inconnu')
+    sys.exit(1)
